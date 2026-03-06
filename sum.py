0cb7765e6f2c8071c7779cfb1d7b9c6c45a61029
Shourob Biswas <shourob623483@gmail.com>
2026-03-06 21:17:49 +0600

    update code

--- sum.py
+++ sum.py
@@ -1,4 +1,4 @@
 a=int(input("enter value for a : "))
 b=int(input("enter value for b : "))
-sum=a+b
-print("sum of these two number is " ,sum )
+Result=a+b
+print("sum of these two number is " ,Result )
